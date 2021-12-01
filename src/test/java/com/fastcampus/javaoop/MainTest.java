e6acab71cf5f816b8dbd740dc6d0f2e45237bb51
Warm Oil <jipmj12@naver.com>
2021-12-01 15:57:02 +0900

    Implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용 + 테스트

---
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -0,0 +1,18 @@
+package com.fastcampus.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //g
+        String[] args =  {"3","2","5"};
+                //when and then
+        Main.main(args);
+
+    }
+
+}
